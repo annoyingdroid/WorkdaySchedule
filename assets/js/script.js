3c9ce4dd3e9d46dc1e6d607562b2f3bbf7e47e3b
unknown <droid64down@gmail.com>
2022-08-31 17:15:31 -0400

    Wrote functional JS code

---
+++ assets/js/script.js
@@ -0,0 +1,70 @@
+
+//Variables
+const date = new Date();
+var index = 0;
+var hours = 9;
+var events = [];
+var scheduleItems = [];
+var curDay = new Date().toLocaleString('en-us', {weekday: 'long'});
+var curDate = new Date().toLocaleString('en-us', {year: 'numeric', month: 'short', day: 'numeric'});
+var curTime = date.getTime();
+var clock;
+var background = Math.round(1 + (Math.random() * 8));
+var imgURL = "assets/images/background\\ " + background + ".jpg";
+
+//Set random background image for header
+$("#header").css({
+    "box-shadow": "0px -210px 300px 25px rgba(255,255,255,1) inset",
+    "background-image":"url(" + imgURL + ")",
+    "background-repeat": "no-repeat",
+    "background-size": "cover",
+    "background-position": "center center"
+});
+
+//Generating hour blocks
+for (var i = 0; i < hours; i++) {
+    events.push(hours + i +":00");
+    $('.container').append(`
+    <div class="row time-block">
+
+    <div class="col-sm col-md-2 hour">
+        ${hours + i + ":00"}
+    </div>
+    <div class="col-sm col-md-10 d-flex description">
+      <div class="input-group">
+          <textarea class="form-control text-area"></textarea>
+          <div class='input-group-append'>
+          <button class='save-button d-flex justify-center align-center'>
+            <i class='far fa-save fa-2x save-icon'></i>
+          </button>
+        </div>
+      </div>
+    </div>
+  </div>
+`);
+}
+
+//Saving an event
+
+//Saving the calendar
+
+//Check for current time & disable if time passed
+
+
+//Set the current day and date
+$("#header h1 #weekday").text(curDay);
+$("#header #longDate").text(curDate);
+
+//Loading the current time
+function timeUpdate(){
+    $("#header #curTime").text(new Date().toLocaleTimeString('en-us', {hour12: false}));
+}
+
+//Loading the number of scheduled items
+$("#header p #schedItems").text(scheduleItems.length);
+
+//Make time dynamically update
+timeUpdate();
+clock = setInterval(timeUpdate, 1000);
+
+//EventListeners
